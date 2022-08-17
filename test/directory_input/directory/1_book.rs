b058f49ee0d5518cf445153c3af5d7d2140737af
James Kessler <james.kessler@forallsecure.com>
2022-08-17 14:20:21 -0600

    Generate jsdoc comments instead of code comments

--- test/directory_input/directory/1_book.rs
+++ test/directory_input/directory/1_book.rs
@@ -4,7 +4,11 @@ use tsync::tsync;
 /// Doc comments are preserved too!
 #[tsync]
 struct Book {
+    /// Name of the book.
     name: String,
+    /// Chapters of the book.
     chapters: Vec<Chapter>,
+    /// Reviews of the book
+    /// by users.
     user_reviews: Option<Vec<String>>,
 }
--- test/file_input/typescript.d.ts
+++ test/file_input/typescript.d.ts
@@ -1,16 +1,27 @@
 /* This file is generated and managed by tsync */
 
-// Doc comments are preserved too!
+/** Doc comments are preserved too! */
 interface Book {
+  /** Name of the book. */
   name: string
+  /** Chapters of the book. */
   chapters: Array<Chapter>
+  /**
+   * Reviews of the book
+   * by users.
+   */
   user_reviews: Array<string> | undefined
 }
 
+/**
+ * Multiple line comments
+ * are formatted on
+ * separate lines
+ */
 interface Chapter {
   title: string
   pages: number
 }
 
-// Time in UTC seconds
+/** Time in UTC seconds */
 type UTC = number

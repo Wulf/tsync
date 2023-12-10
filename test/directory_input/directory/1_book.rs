9a0fffa566fc17a310849af3c52da88d65a8ff04
Aaron Hughes <33838207+ahughes-42@users.noreply.github.com>
2023-12-10 17:21:37 -0500

    Updated test results.

--- test/directory_input/directory/1_book.rs
+++ test/directory_input/directory/1_book.rs
@@ -12,3 +12,17 @@ struct Book {
     /// by users.
     user_reviews: Option<Vec<String>>,
 }
+
+#[tsync]
+#[derive(Serialize)]
+#[serde(rename_all = "camelCase")]
+/// Book struct with camelCase field names.
+struct BookCamel {
+    /// Name of the book.
+    name: String,
+    /// Chapters of the book.
+    chapters: Vec<Chapter>,
+    /// Reviews of the book
+    /// by users.
+    user_reviews: Option<Vec<String>>,
+}
--- test/directory_input/typescript.d.ts
+++ test/directory_input/typescript.d.ts
@@ -1,1 +1,40 @@
 /* This file is generated and managed by tsync */
+
+/** Doc comments are preserved too! */
+interface Book {
+  /** Name of the book. */
+  name: string;
+  /** Chapters of the book. */
+  chapters: Array<Chapter>;
+  /**
+   * Reviews of the book
+   * by users.
+   */
+  user_reviews?: Array<string>;
+}
+
+/** Book struct with camelCase field names. */
+interface BookCamel {
+  /** Name of the book. */
+  name: string;
+  /** Chapters of the book. */
+  chapters: Array<Chapter>;
+  /**
+   * Reviews of the book
+   * by users.
+   */
+  userReviews?: Array<string>;
+}
+
+/**
+ * Multiple line comments
+ * are formatted on
+ * separate lines
+ */
+interface Chapter {
+  title: string;
+  pages: number;
+}
+
+/** Time in UTC seconds */
+type UTC = number

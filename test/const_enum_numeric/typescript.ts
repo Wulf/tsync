272df6d6143b37c5421f540c77016f2c4c80b48b
AnthonyMichaelTDM <68485672+AnthonyMichaelTDM@users.noreply.github.com>
2024-05-08 14:32:52 -0700

    test(const enums): add integration test for const enum generation
    also adds missing `./enum_numeric/tsync.sh` to ./test/test_all.sh

---
+++ test/const_enum_numeric/typescript.ts
@@ -0,0 +1,8 @@
+/* This file is generated and managed by tsync */
+
+/** Integer enums should follow rust discrimination if literals (doesn't evaluate expression) */
+export const enum Foo {
+  Bar = 0,
+  Baz = 123,
+  Quux = 124,
+}
